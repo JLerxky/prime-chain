c068b128a1dbbe977817918ffe18a0ec95faeb38
kaiyong.xie <kaiyong.xie@geely.com>
2021-02-01 15:16:57 +0800

    gui简化

--- gui/src/gui.rs
+++ gui/src/gui.rs
@@ -2,9 +2,8 @@
 #![windows_subsystem = "windows"]
 
 use iced::{
-    button, scrollable, slider, text_input, window, Application, Button, Checkbox, Color, Column,
-    Command, Container, Element, HorizontalAlignment, Length, Radio, Row, Scrollable, Settings,
-    Slider, Space, Text, TextInput,
+    button, scrollable, window, Application, Button, Color, Column, Command, Container, Element,
+    HorizontalAlignment, Length, Row, Scrollable, Settings, Space, Text,
 };
 
 // pub fn main() -> iced::Result {
@@ -62,7 +61,7 @@ impl Application for App {
                 self.window_mode = window::Mode::Windowed;
             }
             EventMessage::RouteMessage(route_msg) => {
-                self.routes.update(route_msg, &mut self.debug);
+                self.routes.update(route_msg);
             }
         }
         Command::none()
@@ -101,7 +100,7 @@ impl Application for App {
             .max_width(540)
             .spacing(20)
             .padding(20)
-            .push(routes.view(self.debug).map(EventMessage::RouteMessage))
+            .push(routes.view().map(EventMessage::RouteMessage))
             .push(controls)
             .into();
 
@@ -140,47 +139,17 @@ struct Routes {
 impl Routes {
     fn new() -> Routes {
         Routes {
-            routes: vec![
-                Route::Welcome,
-                Route::Slider {
-                    state: slider::State::new(),
-                    value: 50,
-                },
-                Route::RowsAndColumns {
-                    layout: Layout::Row,
-                    spacing_slider: slider::State::new(),
-                    spacing: 20,
-                },
-                Route::Text {
-                    size_slider: slider::State::new(),
-                    size: 30,
-                    color_sliders: [slider::State::new(); 3],
-                    color: Color::BLACK,
-                },
-                Route::Radio { selection: None },
-                Route::Image {
-                    width: 300,
-                    slider: slider::State::new(),
-                },
-                Route::Scrollable,
-                Route::TextInput {
-                    value: String::new(),
-                    is_secure: false,
-                    state: text_input::State::new(),
-                },
-                Route::Debugger,
-                Route::End,
-            ],
+            routes: vec![Route::Welcome],
             current: 0,
         }
     }
 
-    fn update(&mut self, msg: RouteMessage, debug: &mut bool) {
-        self.routes[self.current].update(msg, debug);
+    fn update(&mut self, msg: RouteMessage) {
+        self.routes[self.current].update(msg);
     }
 
-    fn view(&mut self, debug: bool) -> Element<RouteMessage> {
-        self.routes[self.current].view(debug)
+    fn view(&mut self) -> Element<RouteMessage> {
+        self.routes[self.current].view()
     }
 
     fn advance(&mut self) {
@@ -210,163 +179,35 @@ impl Routes {
 
 enum Route {
     Welcome,
-    Slider {
-        state: slider::State,
-        value: u8,
-    },
-    RowsAndColumns {
-        layout: Layout,
-        spacing_slider: slider::State,
-        spacing: u16,
-    },
-    Text {
-        size_slider: slider::State,
-        size: u16,
-        color_sliders: [slider::State; 3],
-        color: Color,
-    },
-    Radio {
-        selection: Option<Language>,
-    },
-    Image {
-        width: u16,
-        slider: slider::State,
-    },
-    Scrollable,
-    TextInput {
-        value: String,
-        is_secure: bool,
-        state: text_input::State,
-    },
-    Debugger,
-    End,
 }
 
 #[derive(Debug, Clone)]
 pub enum RouteMessage {
-    SliderChanged(u8),
-    LayoutChanged(Layout),
-    SpacingChanged(u16),
-    TextSizeChanged(u16),
-    TextColorChanged(Color),
-    LanguageSelected(Language),
-    ImageWidthChanged(u16),
-    InputChanged(String),
-    ToggleSecureInput(bool),
-    DebugToggled(bool),
+    Welcome,
 }
 
 impl<'a> Route {
-    fn update(&mut self, msg: RouteMessage, debug: &mut bool) {
+    fn update(&mut self, msg: RouteMessage) {
         match msg {
-            RouteMessage::DebugToggled(value) => {
-                if let Route::Debugger = self {
-                    *debug = value;
-                }
-            }
-            RouteMessage::LanguageSelected(language) => {
-                if let Route::Radio { selection } = self {
-                    *selection = Some(language);
-                }
-            }
-            RouteMessage::SliderChanged(new_value) => {
-                if let Route::Slider { value, .. } = self {
-                    *value = new_value;
-                }
-            }
-            RouteMessage::TextSizeChanged(new_size) => {
-                if let Route::Text { size, .. } = self {
-                    *size = new_size;
-                }
-            }
-            RouteMessage::TextColorChanged(new_color) => {
-                if let Route::Text { color, .. } = self {
-                    *color = new_color;
-                }
-            }
-            RouteMessage::LayoutChanged(new_layout) => {
-                if let Route::RowsAndColumns { layout, .. } = self {
-                    *layout = new_layout;
-                }
-            }
-            RouteMessage::SpacingChanged(new_spacing) => {
-                if let Route::RowsAndColumns { spacing, .. } = self {
-                    *spacing = new_spacing;
-                }
-            }
-            RouteMessage::ImageWidthChanged(new_width) => {
-                if let Route::Image { width, .. } = self {
-                    *width = new_width;
-                }
-            }
-            RouteMessage::InputChanged(new_value) => {
-                if let Route::TextInput { value, .. } = self {
-                    *value = new_value;
-                }
-            }
-            RouteMessage::ToggleSecureInput(toggle) => {
-                if let Route::TextInput { is_secure, .. } = self {
-                    *is_secure = toggle;
-                }
-            }
+            RouteMessage::Welcome => (),
         };
     }
 
     fn title(&self) -> &str {
         match self {
             Route::Welcome => "Welcome",
-            Route::Radio { .. } => "Radio button",
-            Route::Slider { .. } => "Slider",
-            Route::Text { .. } => "Text",
-            Route::Image { .. } => "Image",
-            Route::RowsAndColumns { .. } => "Rows and columns",
-            Route::Scrollable => "Scrollable",
-            Route::TextInput { .. } => "Text input",
-            Route::Debugger => "Debugger",
-            Route::End => "End",
         }
     }
 
     fn can_continue(&self) -> bool {
         match self {
             Route::Welcome => true,
-            Route::Radio { selection } => *selection == Some(Language::Rust),
-            Route::Slider { .. } => true,
-            Route::Text { .. } => true,
-            Route::Image { .. } => true,
-            Route::RowsAndColumns { .. } => true,
-            Route::Scrollable => true,
-            Route::TextInput { value, .. } => !value.is_empty(),
-            Route::Debugger => true,
-            Route::End => false,
         }
     }
 
-    fn view(&mut self, debug: bool) -> Element<RouteMessage> {
+    fn view(&mut self) -> Element<RouteMessage> {
         match self {
             Route::Welcome => Self::welcome(),
-            Route::Radio { selection } => Self::radio(*selection),
-            Route::Slider { state, value } => Self::slider(state, *value),
-            Route::Text {
-                size_slider,
-                size,
-                color_sliders,
-                color,
-            } => Self::text(size_slider, *size, color_sliders, *color),
-            Route::Image { width, slider } => Self::image(*width, slider),
-            Route::RowsAndColumns {
-                layout,
-                spacing_slider,
-                spacing,
-            } => Self::rows_and_columns(*layout, spacing_slider, *spacing),
-            Route::Scrollable => Self::scrollable(),
-            Route::TextInput {
-                value,
-                is_secure,
-                state,
-            } => Self::text_input(value, *is_secure, state),
-            Route::Debugger => Self::debugger(debug),
-            Route::End => Self::end(),
         }
         .into()
     }
@@ -403,287 +244,6 @@ impl<'a> Route {
                  end!",
             ))
     }
-
-    fn slider(state: &'a mut slider::State, value: u8) -> Column<'a, RouteMessage> {
-        Self::container("Slider")
-            .push(Text::new(
-                "A slider allows you to smoothly select a value from a range \
-                 of values.",
-            ))
-            .push(Text::new(
-                "The following slider lets you choose an integer from \
-                 0 to 100:",
-            ))
-            .push(Slider::new(
-                state,
-                0..=100,
-                value,
-                RouteMessage::SliderChanged,
-            ))
-            .push(
-                Text::new(&value.to_string())
-                    .width(Length::Fill)
-                    .horizontal_alignment(HorizontalAlignment::Center),
-            )
-    }
-
-    fn rows_and_columns(
-        layout: Layout,
-        spacing_slider: &'a mut slider::State,
-        spacing: u16,
-    ) -> Column<'a, RouteMessage> {
-        let row_radio = Radio::new(
-            Layout::Row,
-            "Row",
-            Some(layout),
-            RouteMessage::LayoutChanged,
-        );
-
-        let column_radio = Radio::new(
-            Layout::Column,
-            "Column",
-            Some(layout),
-            RouteMessage::LayoutChanged,
-        );
-
-        let layout_section: Element<_> = match layout {
-            Layout::Row => Row::new()
-                .spacing(spacing)
-                .push(row_radio)
-                .push(column_radio)
-                .into(),
-            Layout::Column => Column::new()
-                .spacing(spacing)
-                .push(row_radio)
-                .push(column_radio)
-                .into(),
-        };
-
-        let spacing_section = Column::new()
-            .spacing(10)
-            .push(Slider::new(
-                spacing_slider,
-                0..=80,
-                spacing,
-                RouteMessage::SpacingChanged,
-            ))
-            .push(
-                Text::new(&format!("{} px", spacing))
-                    .width(Length::Fill)
-                    .horizontal_alignment(HorizontalAlignment::Center),
-            );
-
-        Self::container("Rows and columns")
-            .spacing(spacing)
-            .push(Text::new(
-                "Iced uses a layout model based on flexbox to position UI \
-                 elements.",
-            ))
-            .push(Text::new(
-                "Rows and columns can be used to distribute content \
-                 horizontally or vertically, respectively.",
-            ))
-            .push(layout_section)
-            .push(Text::new(
-                "You can also easily change the spacing between elements:",
-            ))
-            .push(spacing_section)
-    }
-
-    fn text(
-        size_slider: &'a mut slider::State,
-        size: u16,
-        color_sliders: &'a mut [slider::State; 3],
-        color: Color,
-    ) -> Column<'a, RouteMessage> {
-        let size_section = Column::new()
-            .padding(20)
-            .spacing(20)
-            .push(Text::new("You can change its size:"))
-            .push(Text::new(&format!("This text is {} pixels", size)).size(size))
-            .push(Slider::new(
-                size_slider,
-                10..=70,
-                size,
-                RouteMessage::TextSizeChanged,
-            ));
-
-        let [red, green, blue] = color_sliders;
-
-        let color_sliders = Row::new()
-            .spacing(10)
-            .push(color_slider(red, color.r, move |r| Color { r, ..color }))
-            .push(color_slider(green, color.g, move |g| Color { g, ..color }))
-            .push(color_slider(blue, color.b, move |b| Color { b, ..color }));
-
-        let color_section = Column::new()
-            .padding(20)
-            .spacing(20)
-            .push(Text::new("And its color:"))
-            .push(Text::new(&format!("{:?}", color)).color(color))
-            .push(color_sliders);
-
-        Self::container("Text")
-            .push(Text::new(
-                "Text is probably the most essential widget for your UI. \
-                 It will try to adapt to the dimensions of its container.",
-            ))
-            .push(size_section)
-            .push(color_section)
-    }
-
-    fn radio(selection: Option<Language>) -> Column<'a, RouteMessage> {
-        let question = Column::new()
-            .padding(20)
-            .spacing(10)
-            .push(Text::new("Iced is written in...").size(24))
-            .push(Language::all().iter().cloned().fold(
-                Column::new().padding(10).spacing(20),
-                |choices, language| {
-                    choices.push(Radio::new(
-                        language,
-                        language,
-                        selection,
-                        RouteMessage::LanguageSelected,
-                    ))
-                },
-            ));
-
-        Self::container("Radio button")
-            .push(Text::new(
-                "A radio button is normally used to represent a choice... \
-                 Surprise test!",
-            ))
-            .push(question)
-            .push(Text::new(
-                "Iced works very well with iterators! The list above is \
-                 basically created by folding a column over the different \
-                 choices, creating a radio button for each one of them!",
-            ))
-    }
-
-    fn image(width: u16, slider: &'a mut slider::State) -> Column<'a, RouteMessage> {
-        Self::container("Image")
-            .push(Text::new("An image that tries to keep its aspect ratio."))
-            .push(ferris(width))
-            .push(Slider::new(
-                slider,
-                100..=500,
-                width,
-                RouteMessage::ImageWidthChanged,
-            ))
-            .push(
-                Text::new(&format!("Width: {} px", width.to_string()))
-                    .width(Length::Fill)
-                    .horizontal_alignment(HorizontalAlignment::Center),
-            )
-    }
-
-    fn scrollable() -> Column<'a, RouteMessage> {
-        Self::container("Scrollable")
-            .push(Text::new(
-                "Iced supports scrollable content. Try it out! Find the \
-                 button further below.",
-            ))
-            .push(Text::new("Tip: You can use the scrollbar to scroll down faster!").size(16))
-            .push(Column::new().height(Length::Units(4096)))
-            .push(
-                Text::new("You are halfway there!")
-                    .width(Length::Fill)
-                    .size(30)
-                    .horizontal_alignment(HorizontalAlignment::Center),
-            )
-            .push(Column::new().height(Length::Units(4096)))
-            .push(ferris(300))
-            .push(
-                Text::new("You made it!")
-                    .width(Length::Fill)
-                    .size(50)
-                    .horizontal_alignment(HorizontalAlignment::Center),
-            )
-    }
-
-    fn text_input(
-        value: &str,
-        is_secure: bool,
-        state: &'a mut text_input::State,
-    ) -> Column<'a, RouteMessage> {
-        let text_input = TextInput::new(
-            state,
-            "Type something to continue...",
-            value,
-            RouteMessage::InputChanged,
-        )
-        .padding(10)
-        .size(30);
-        Self::container("Text input")
-            .push(Text::new(
-                "Use a text input to ask for different kinds of information.",
-            ))
-            .push(if is_secure {
-                text_input.password()
-            } else {
-                text_input
-            })
-            .push(Checkbox::new(
-                is_secure,
-                "Enable password mode",
-                RouteMessage::ToggleSecureInput,
-            ))
-            .push(Text::new(
-                "A text input produces a message every time it changes. It is \
-                 very easy to keep track of its contents:",
-            ))
-            .push(
-                Text::new(if value.is_empty() {
-                    "You have not typed anything yet..."
-                } else {
-                    value
-                })
-                .width(Length::Fill)
-                .horizontal_alignment(HorizontalAlignment::Center),
-            )
-    }
-
-    fn debugger(debug: bool) -> Column<'a, RouteMessage> {
-        Self::container("Debugger")
-            .push(Text::new(
-                "You can ask Iced to visually explain the layouting of the \
-                 different elements comprising your UI!",
-            ))
-            .push(Text::new(
-                "Give it a shot! Check the following checkbox to be able to \
-                 see element boundaries.",
-            ))
-            .push(if cfg!(target_arch = "wasm32") {
-                Element::new(
-                    Text::new("Not available on web yet!")
-                        .color([0.7, 0.7, 0.7])
-                        .horizontal_alignment(HorizontalAlignment::Center),
-                )
-            } else {
-                Element::new(Checkbox::new(
-                    debug,
-                    "Explain layout",
-                    RouteMessage::DebugToggled,
-                ))
-            })
-            .push(Text::new("Feel free to go back and take a look."))
-    }
-
-    fn end() -> Column<'a, RouteMessage> {
-        Self::container("You reached the end!")
-            .push(Text::new(
-                "This tour will be updated as more features are added.",
-            ))
-            .push(Text::new("Make sure to keep an eye on it!"))
-    }
-}
-
-fn ferris<'a>(width: u16) -> Container<'a, RouteMessage> {
-    Container::new(Text::new("ferris").width(Length::FillPortion(width)))
-        .width(Length::Fill)
-        .center_x()
 }
 
 fn button<'a, Message: Clone>(state: &'a mut button::State, label: &str) -> Button<'a, Message> {
@@ -693,53 +253,6 @@ fn button<'a, Message: Clone>(state: &'a mut button::State, label: &str) -> Butt
     )
     .padding(12)
     .min_width(100)
-}
-
-fn color_slider(
-    state: &mut slider::State,
-    component: f32,
-    update: impl Fn(f32) -> Color + 'static,
-) -> Slider<f64, RouteMessage> {
-    Slider::new(state, 0.0..=1.0, f64::from(component), move |c| {
-        RouteMessage::TextColorChanged(update(c as f32))
-    })
-    .step(0.01)
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Language {
-    Rust,
-    Elm,
-    Ruby,
-    Haskell,
-    C,
-    Other,
-}
-
-impl Language {
-    fn all() -> [Language; 6] {
-        [
-            Language::C,
-            Language::Elm,
-            Language::Ruby,
-            Language::Haskell,
-            Language::Rust,
-            Language::Other,
-        ]
-    }
-}
-
-impl From<Language> for String {
-    fn from(language: Language) -> String {
-        String::from(match language {
-            Language::Rust => "Rust",
-            Language::Elm => "Elm",
-            Language::Ruby => "Ruby",
-            Language::Haskell => "Haskell",
-            Language::C => "C",
-            Language::Other => "Other",
-        })
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
