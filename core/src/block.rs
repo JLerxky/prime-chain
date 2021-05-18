09f508a0faaf9ab9bf7442593cd831414daa6c06
JLer <472102489@qq.com>
2021-05-18 17:30:30 +0800

    区块存储数据泛型化

--- core/src/block.rs
+++ core/src/block.rs
@@ -1,14 +1,14 @@
-use crate::tile::Tile;
 use serde::{Deserialize, Serialize};
-use util::sleder::Sleder;
-#[derive(Default, Debug, Serialize, Deserialize, Clone)]
-pub struct Block {
+
+/// 区块
+#[derive(Default, Debug, Serialize, Deserialize, Clone, Copy)]
+pub struct Block<T> {
     pub header: BlockHeader,
     pub hash: [u8; 32],
-    pub body: [Tile; 27],
+    pub body: T,
 }
 
-#[derive(Default, Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+#[derive(Default, Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
 pub struct BlockHeader {
     pub height: u64,
     pub time: i64,
@@ -16,25 +16,15 @@ pub struct BlockHeader {
     pub pre_hash: [u8; 32],
 }
 
-impl Block {
-    /// 创建新区块
-    pub fn new() -> Self {
-        Block::default()
-    }
-
-    /// 创建创世区块
-    pub fn new_genesis_block() -> Self {
-        // body
-        let body = [Tile {
-            point: (0, 0, 0),
-            tile_type: 0,
-        }; 27];
+impl<T: Default + Serialize> Block<T> {
+    /// 创建区块
+    pub fn new(height: u64, body: T) -> Self {
         let mut body_hash: [u8; 32] = [0; 32];
         util::coder::get_hash(&util::coder::serialize(&body)[..], &mut body_hash);
 
         // header
         let header = BlockHeader {
-            height: 1,
+            height,
             time: chrono::Utc::now().timestamp(),
             body_hash,
             pre_hash: [0; 32],
@@ -42,17 +32,6 @@ impl Block {
         let mut hash: [u8; 32] = [0; 32];
         util::coder::get_hash(&util::coder::serialize(&header)[..], &mut hash);
 
-        let genesis_block = Block { header, hash, body };
-        // 持久化创世块
-        Block::save_block(&genesis_block);
-        genesis_block
-    }
-
-    fn save_block(block: &Block) {
-        if let Some(sled_db) = Sleder::open() {
-            sled_db
-                .insert(block.hash, util::coder::serialize(block))
-                .unwrap();
-        }
+        Block { header, hash, body }
     }
 }
--- core/src/blockchain.rs
+++ core/src/blockchain.rs
@@ -1,6 +1,9 @@
 use util::sleder::Sleder;
 
-use crate::block::Block;
+use crate::{
+    block::{Block, BlockHeader},
+    tile::Tile,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Serialize, Deserialize, Clone)]
@@ -13,7 +16,7 @@ pub struct BlockChain {
 impl BlockChain {
     /// 创建新链
     pub fn new() -> Self {
-        let genesis_block = crate::block::Block::new_genesis_block();
+        let genesis_block: Block<[[[Tile; 3]; 3]; 3]> = BlockChain::new_genesis_block();
 
         let blockchain = BlockChain {
             genesis_hash: genesis_block.hash,
@@ -26,15 +29,58 @@ impl BlockChain {
     }
 
     /// 写入新区块
-    fn write_block(block: Block) {
+    pub fn write_block<T>(block: Block<T>) {
         // 校验
         // 持久化
     }
 
+    /// 创建创世区块
+    fn new_genesis_block() -> Block<[[[Tile; 3]; 3]; 3]> {
+        // body
+        let body = [[[Tile {
+            point: (0, 0, 0),
+            tile_type: 0,
+        }; 3]; 3]; 3];
+        let height = 1u64;
+
+        let genesis_block = Block::new(height, body);
+        // 持久化创世块
+        BlockChain::save_block(&genesis_block);
+        genesis_block
+    }
+
+    fn save_block<T: Serialize>(block: &Block<T>) {
+        if let Some(sled_db) = Sleder::open() {
+            sled_db
+                .insert(
+                    format!("block-{:?}", block.hash),
+                    util::coder::serialize(block),
+                )
+                .unwrap();
+        }
+    }
+
+    fn save_tail_block<T: Serialize>(block: &Block<T>) {
+        if let Some(sled_db) = Sleder::open() {
+            sled_db
+                .insert("block-tail", util::coder::serialize(block))
+                .unwrap();
+        }
+    }
+
+    // fn get_tail_block<T: Serialize + Deserialize<'static>>() -> Option<Block<T>> {
+    //     if let Some(sled_db) = Sleder::open() {
+    //         if let Ok(Some(bytes)) = sled_db.get("block-tail") {
+    //             return Some(util::coder::deserialize(&bytes));
+    //         }
+    //     }
+    //     None
+    // }
+
     fn save_blockchain(blockchain: &BlockChain) {
         if let Some(sled_db) = Sleder::open() {
             sled_db
-                .insert(blockchain.genesis_hash, util::coder::serialize(blockchain))
+                .insert("blockchain", util::coder::serialize(blockchain))
                 .unwrap();
         }
     }
